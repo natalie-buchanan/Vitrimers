e3579ebce336613f492521fd6841775102a6a738
Natalie Buchanan <natalie.buchanan1@gmail.com>
2024-11-12 14:33:09 -0500

    Used 3D angle method in 2D case

--- beads_from_edge_2D.py
+++ beads_from_edge_2D.py
@@ -19,7 +19,6 @@ def load_files(name: str, network="auelp"):
     base_path = os.path.join(os.getcwd(), "Data", network)
     core_x = np.loadtxt(os.path.join(base_path, name + '-core_x.dat'))
     core_y = np.loadtxt(os.path.join(base_path, name + '-core_y.dat'))
-    core_z = np.loadtxt(os.path.join(base_path, name + '-core_z.dat'))
     edges = np.loadtxt(os.path.join(base_path, name + '-conn_core_edges.dat'))
     pb_edges = np.loadtxt(os.path.join(base_path, name + '-conn_pb_edges.dat'))
     node_type = np.loadtxt(os.path.join(base_path, name +
@@ -28,27 +27,60 @@ def load_files(name: str, network="auelp"):
     # subtract 1 to get number of connecting beads from number of bonds
     len_of_chain = int(np.loadtxt(os.path.join(base_path, name[0:-3] +
                                                '-n.dat'))) - 1
-    node_files = np.asarray([core_x, core_y, core_z, node_type])
+    node_files = np.asarray([core_x, core_y, node_type])
     node_files = node_files.transpose()
 
     return [node_files, edges, pb_edges, box_size, len_of_chain]
 
 
-def create_neighborhood(x, y, z, factor=1, number=9):
+def write_LAMMPS_data(name: str, atom_data, bond_data, box_size,
+                      network="auelp"):
+    """Write LAMMPS in.data file including positions and bonds."""
+    base_path = os.path.join(os.getcwd(), "Data", network)
+    data_file = os.path.join(base_path, name + '-in.data')
+    with open(data_file, "w",  encoding="utf-8") as file:
+        file.write("""{name}
+
+{num_atoms} atoms
+{num_bonds} bond
+
+0 {L_box} xlo xhi
+0 {L_box} ylo yhi
+
+Masses\n\n""".format(name=name, num_atoms=len(atom_data),
+                   num_bonds=len(bond_data), L_box=box_size))
+        for i in atom_data["atom-type"].drop_duplicates().sort_values():
+            file.write(f'{int(i)} 1\n')
+        file.write("""
+Atoms # ID molID type x y z\n\n""")
+        for i in atom_data.index:
+            file.write(f"{int(atom_data.iloc[i]['ID']+1)} " +
+                       f"{int(atom_data.iloc[i]['Mol']+1)} " +
+                       f"{int(atom_data.iloc[i]['atom-type'])} " +
+                       f"{atom_data.iloc[i]['X']} " +
+                       f"{atom_data.iloc[i]['Y']} " +
+                       "0 \n")
+        file.write("""
+Bonds\n\n""")
+        for i in bond_data.index:
+            file.write(f"{i + 1} " +
+                       f"{int(bond_data.iloc[i]['BondType'])} " +
+                       f"{int(bond_data.iloc[i]['Atom1'])} " +
+                       f"{int(bond_data.iloc[i]['Atom2'])} " +
+                       "\n")
+
+
+def create_neighborhood(x, y, factor=0.97, number=9):
     """Create neighborhood."""
     points = []
     # Adjust number for finer granularity
     for theta in np.linspace(0, 2*np.pi, number):
-        # Adjust number for finer granularity
-        for phi in np.linspace(0, np.pi, number):
+            xn = x + factor * np.cos(theta)
+            yn = y + factor * np.sin(theta)
 
-            xn = x + factor * np.sin(phi) * np.cos(theta)
-            yn = y + factor * np.sin(phi) * np.sin(theta)
-            zn = z + factor * np.cos(phi)
-
-            if all(v == 0 for v in [xn, yn, zn]):
+            if all(v == 0 for v in [xn, yn]):
                 print("Here")
-            points.append([xn, yn, zn])
+            points.append([xn, yn])
     points = np.array(points)
 
     return np.array(points)
@@ -102,14 +134,12 @@ def check_distances(A, B, distance, box_size):
         for j in range(B.shape[0]):
             dx = np.abs(A[i, 0] - B[j, 0])
             dy = np.abs(A[i, 1] - B[j, 1])
-            dz = np.abs(A[i, 2] - B[j, 2])
 
             # Periodic boundary conditions
             dx = np.minimum(dx, box_size - dx)
             dy = np.minimum(dy, box_size - dy)
-            dz = np.minimum(dz, box_size - dz)
 
-            dist = np.sqrt(dx**2 + dy**2 + dz**2)
+            dist = np.sqrt(dx**2 + dy**2)
 
             if dist < distance:
                 is_close[i] = True
@@ -117,40 +147,31 @@ def check_distances(A, B, distance, box_size):
     return is_close
 
 
-def check_neighborhood(neighborhood, bead_positions, cutoff=0.5):
+def check_neighborhood(neighborhood, bead_positions, cutoff=0.25):
     """Check to see if neighbors are occupied."""
     truth = check_distances(neighborhood, bead_positions, cutoff, BOX_SIZE)
-    # for j, pair in enumerate(neighborhood):
-    #     if not pair.all():
-    #         neighborhood[j] = False
-    #     else:
-    #         test = pd.DataFrame(bead_positions, columns=["X", "Y", "Z"])
-    #         test.dropna(axis=0, inplace=True)
-    #         unwrap_positions = test.apply(unwrap_coords, axis=1,
-    #                                        second_point=pair, L=BOX_SIZE,
-    #                                        result_type='broadcast')
-    #         check = abs(unwrap_positions - pair) <= cutoff
-    #         if check.any().all():
-    #             neighborhood[j] = False
-    # If all occupied, returns False, else returns neighborhood
     test = neighborhood[~truth]
     if not test.any():
         return False
     return test
 
 
-def find_neighborhood(x, y, z, bead_positions):
+def find_neighborhood(x, y, bead_positions):
     """Create neighborhood that is not fully occupied."""
-    neighborhood = create_neighborhood(x, y, z)
+    neighborhood = create_neighborhood(x, y)
     check = check_neighborhood(neighborhood, bead_positions)
     if not isinstance(check, np.ndarray):
         print('Here')
         # generate neighborhood of points 1.5 away if all 1 away are occupied
-        neighborhood = create_neighborhood(x, y, z, number=4)
-        if not isinstance(check_neighborhood(neighborhood, bead_positions),
-                          np.ndarray):
-            raise ValueError(
-                'Neighborhood full: ' + str(neighborhood))
+        numbertries = [3, 4, 5, 11, 13, 16, 20]
+        while len(numbertries) > 0:
+            num_index = random.randint(0, len(numbertries)-1)
+            interval = numbertries.pop(num_index)
+            neighborhood = create_neighborhood(x, y, number=interval)
+            if not isinstance(check_neighborhood(neighborhood, bead_positions),
+                            np.ndarray):
+                raise ValueError(
+                    'Neighborhood full: ' + str(neighborhood))
     return neighborhood
 
 
@@ -158,7 +179,7 @@ def select_indices(theta, neighborhood, current_point):
     """Select indices based on theta and neighborhood."""
     random.seed(42)
     valid_indices = np.where(np.any(neighborhood != 0, axis=1))[0]
-    indices = np.empty([len(valid_indices), 3])
+    indices = np.empty([len(valid_indices), 2])
     for i in range(neighborhood.shape[1]):
         column = neighborhood[valid_indices, 1]
         if random.random() < theta[i]:
@@ -170,23 +191,22 @@ def select_indices(theta, neighborhood, current_point):
 
 def calculate_theta(current_point, target_point, n, i):
     """Calculate chance of moving in positive or negative direction."""
-    x, y, z = current_point
-    xtarg, ytarg, ztarg = target_point
+    x, y = current_point
+    xtarg, ytarg= target_point
     denom = (0.97 / 2.0) * (n - (i * 2))
 
     theta_x = 0.5 * (1 - ((xtarg - x) / denom))
     theta_y = 0.5 * (1 - ((ytarg - y) / denom))
-    theta_z = 0.5 * (1 - ((ztarg - z) / denom))
-    return theta_x, theta_y, theta_z
+    return theta_x, theta_y
 
 
 def step_choice(i, current_point, target_point, bead_positions, n):
     """Choose next step in constrained walk."""
-    x, y, z = current_point
+    x, y = current_point
 
     theta = calculate_theta(current_point, target_point, n, i)
 
-    region = find_neighborhood(x, y, z, bead_positions)
+    region = find_neighborhood(x, y, bead_positions)
     rows = np.any(region != 0, axis=1)
     region = region[rows]
     index = select_indices(theta, region, current_point)
@@ -204,29 +224,27 @@ def constrained_walk(start, end, n=15):
     """Generate atom position in a constrained walk between two nodes."""
     [x0, xn] = wrap_coords([start[0], end[0]], BOX_SIZE)
     [y0, yn] = wrap_coords([start[1], end[1]], BOX_SIZE)
-    [z0, zn] = wrap_coords([start[2], end[2]], BOX_SIZE)
     x = x0
     y = y0
-    z = z0
-    bead_positions = np.empty((n+1, 3))
+    bead_positions = np.empty((n+1, 2))
     bead_positions[:] = np.nan
-    bead_positions[0] = [x0, y0, z0]
-    bead_positions[-1] = [xn, yn, zn]
+    bead_positions[0] = [x0, y0]
+    bead_positions[-1] = [xn, yn]
 
-    for i in range(int(n/2)):
+    for i in range(int(n+2/2)):
         # Step from starting side
-        current_point = x, y, z
-        target_point = xn, yn, zn
+        current_point = x, y
+        target_point = xn, yn
         current_point = unwrap_coords(current_point, target_point, BOX_SIZE)
-        x, y, z = step_choice(i, current_point, target_point,
+        x, y = step_choice(i, current_point, target_point,
                               bead_positions, n)
-        bead_positions[i+1] = wrap_coords([x, y, z], BOX_SIZE)
+        bead_positions[i+1] = wrap_coords([x, y], BOX_SIZE)
         # Step from ending side
-        current_point = xn, yn, zn
-        target_point = x, y, zn
-        xn, yn, zn = step_choice(i, current_point, target_point,
+        current_point = xn, yn
+        target_point = x, y
+        xn, yn = step_choice(i, current_point, target_point,
                                  bead_positions, n)
-        bead_positions[n-i-1] = wrap_coords([xn, yn, zn], BOX_SIZE)
+        bead_positions[n + 1 - i] = wrap_coords([xn, yn], BOX_SIZE)
     return bead_positions[1:-1]
 
 
@@ -252,24 +270,20 @@ def calculate_wrapped_distance_full(array):
     for i in range(0, num_rows - 1, 1):
         new_row = unwrap_coords(array[i], array[i+1], BOX_SIZE)
         results[i] = math.sqrt((new_row[0]-array[i + 1, 0])**2 +
-                               (new_row[1] - array[i + 1, 1])**2 +
-                               (new_row[2] - array[i + 1, 2])**2)
+                               (new_row[1] - array[i + 1, 1])**2)
     return results
 
 
-def create_atom_list(node_data, edge_data):
+def create_atom_list(node_data, edge_data, length_of_chain):
     """Create panda Data Frame of generated positions and nodes."""
-    x_data, y_data, z_data, node_type = node_data[:,
-                                                  0], node_data[:, 1], node_data[:, 2], node_data[:, 3]
+    x_data, y_data, node_type = node_data[:,0], node_data[:, 1], node_data[:, 2]
     atom_list = pd.DataFrame(data={'ID':
-                                   np.arange(0, ((LENGTH_OF_CHAIN) *
+                                   np.arange(0, ((length_of_chain) *
                                                  len(edge_data) +
                                                  len(x_data)), 1),
-                                   'X': np.nan, 'Y': np.nan, 'Z': np.nan,
-                                   'Mol': np.nan})
+                                   'X': np.nan, 'Y': np.nan, 'Mol': np.nan})
     atom_list.loc[np.arange(0, len(x_data)), "X"] = x_data
     atom_list.loc[np.arange(0, len(x_data)), "Y"] = y_data
-    atom_list.loc[np.arange(0, len(x_data)), "Z"] = z_data
     atom_list.loc[np.arange(0, len(x_data)), "atom-type"] = node_type
     atom_list.loc[np.arange(0, len(x_data)), "Mol"] = 0
     return atom_list
@@ -287,23 +301,19 @@ def update_bond_list(bead_ids, node_id, bond_list):
 
 def create_chains(full_edge_data, bond_data, bead_data, node_data):
     """Create chains."""
-    [node_x, node_y, node_z, node_type] = [node_data[:, 0], node_data[:, 1],
-                                           node_data[:, 2], node_data[:, 3]]
+    [node_x, node_y, node_type] = [node_data[:, 0], node_data[:, 1], node_data[:, 2]]
     colors = mpl.colormaps['rainbow'](np.linspace(0, 1, len(full_edge_data)))
-    fig = plt.figure()
-    ax = fig.add_subplot(projection='3d')
+    plt.figure()
     fullcycle = pd.DataFrame(data=np.zeros([len(full_edge_data), 2]),
                              columns=["Cycles", "Max Dist"])
 
     for chain, edge in enumerate(full_edge_data):
         print(chain)
-        point_0 = (node_x[int(edge[0])], node_y[int(edge[0])],
-                   node_z[int(edge[0])])
-        point_n = (node_x[int(edge[1])], node_y[int(edge[1])],
-                   node_z[int(edge[1])])
+        point_0 = (node_x[int(edge[0])], node_y[int(edge[0])])
+        point_n = (node_x[int(edge[1])], node_y[int(edge[1])])
         maxdist = 50  # arbitrary large value to start
         cycle = 0
-        while maxdist > 3 and cycle < 25:  # arbitrary cut offs
+        while maxdist > 1 and cycle < 25:  # arbitrary cut offs
             path = constrained_walk(start=point_0, end=point_n,
                                     n=LENGTH_OF_CHAIN)
             curr = max(calculate_wrapped_distance_full(path))[0]
@@ -314,17 +324,15 @@ def create_chains(full_edge_data, bond_data, bead_data, node_data):
             maxdist = min(maxdist, curr)
         path_x = masterpath[:, 0]
         path_y = masterpath[:, 1]
-        path_z = masterpath[:, 2]
         fullcycle.iloc[chain] = [cycle, maxdist]
-        ax.scatter(path_x, path_y, path_z, color=colors[chain], marker='o')
+        plt.scatter(path_x, path_y, color=colors[chain], marker='o')
         # ax.plot(path_x, path_y, path_z, color=colors[chain], linestyle='-')
-        ax.scatter(point_0[0], point_0[1], point_0[2], color='k', marker='>')
-        ax.scatter(point_n[0], point_n[1], point_n[2], color='k', marker='>')
+        plt.scatter(point_0[0], point_0[1], color='k', marker='>')
+        plt.scatter(point_n[0], point_n[1], color='k', marker='>')
         id_range = np.arange(len(node_x) + (chain * len(path_x)),
                              len(node_x) + ((chain + 1) * len(path_x)))
         bead_data.loc[id_range, "X"] = path_x
         bead_data.loc[id_range, "Y"] = path_y
-        bead_data.loc[id_range, "Z"] = path_z
         bead_data.loc[id_range, "Mol"] = chain + 1
         bead_data.loc[id_range, "atom-type"] = 2
         bond_data = update_bond_list(id_range, edge, bond_data)
@@ -333,8 +341,7 @@ def create_chains(full_edge_data, bond_data, bead_data, node_data):
             plt.gca().set_aspect('equal')
             plt.show()
             print(max(calculate_wrapped_distance(path_x)),
-                  max(calculate_wrapped_distance(path_y)),
-                  max(calculate_wrapped_distance(path_z)))
+                  max(calculate_wrapped_distance(path_y)))
             raise ValueError('Error in chain ' + str(chain)
                              + " Max Dist: " + str(maxdist))
     plt.xlabel("X")
@@ -351,37 +358,46 @@ STUDY_NAME = '20241016B1C1'
 [NodeData, Edges, PB_edges, BOX_SIZE, LENGTH_OF_CHAIN] = load_files(STUDY_NAME)
 FullEdges = np.concatenate((Edges, PB_edges))
 FullEdges = FullEdges[0:10]
-BeadData = create_atom_list(NodeData, FullEdges)
+BeadData = create_atom_list(NodeData, FullEdges, LENGTH_OF_CHAIN)
 BondData = pd.DataFrame(columns=["BondType", "Atom1", "Atom2"], dtype="int")
 BeadData, BondData, runInfo = create_chains(FullEdges, BondData, BeadData,
                                             NodeData)
+write_LAMMPS_data(STUDY_NAME, BeadData, BondData, BOX_SIZE)
 
 # %% PLot
 colors = mpl.colormaps['rainbow'](np.linspace(0, 1, len(BeadData)))
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
+plt.figure()
 # ax.scatter(BeadData[BeadData['Mol'] == 0]["X"],
 #            BeadData[BeadData['Mol'] == 0]["Y"],
 #            BeadData[BeadData['Mol'] == 0]["Z"],
 #            marker='>', color='k')
-ax.scatter(BeadData[BeadData['Mol'] == 10]["X"],
+plt.scatter(BeadData[BeadData['Mol'] == 10]["X"],
            BeadData[BeadData['Mol'] == 10]["Y"],
-           BeadData[BeadData['Mol'] == 10]["Z"],
            marker='.', c=colors[BeadData['Mol'] == 10], alpha=0.7)
-
 plt.show()
 
 
 colors = mpl.colormaps['rainbow'](np.linspace(0, 1, len(BeadData)))
-fig = plt.figure()
-ax = fig.add_subplot(projection='3d')
-ax.scatter(BeadData[BeadData['Mol'] == 0]["X"],
+plt.figure()
+plt.scatter(BeadData[BeadData['Mol'] == 0]["X"],
            BeadData[BeadData['Mol'] == 0]["Y"],
-           BeadData[BeadData['Mol'] == 0]["Z"],
            marker='>', color='k')
-ax.plot(BeadData[BeadData['Mol'] == 10]["X"],
+plt.plot(BeadData[BeadData['Mol'] == 10]["X"],
         BeadData[BeadData['Mol'] == 10]["Y"],
-        BeadData[BeadData['Mol'] == 10]["Z"],
         marker='.')
+plt.show()
 
+# %%
+colors = mpl.colormaps['rainbow'](np.linspace(0, 1, len(BeadData)))
+node_x = NodeData[:, 0]
+node_y = NodeData[:, 1]
+Node = [node_x[int(FullEdges[-1][0])], node_y[int(FullEdges[-1][0])]]
+Node2 = [node_x[int(FullEdges[-1][1])], node_y[int(FullEdges[-1][1])]]
+Node2 = unwrap_coords(Node2, Node, BOX_SIZE)
+tester = BeadData[BeadData['Mol'] == 10][["X", "Y"]]
+tester = tester.apply(unwrap_coords, axis=1, result_type='broadcast', second_point=Node, L=BOX_SIZE)
+plt.figure()
+plt.plot(tester["X"],tester["Y"],marker='.')
+plt.scatter(Node[0], Node[1], c='k', marker='o')
+plt.scatter(Node2[0], Node2[1], c='k', marker='*')
 plt.show()
--- beads_using_tris_and_traps.py
+++ beads_using_tris_and_traps.py
@@ -33,11 +33,19 @@ def load_files(name: str, network="auelp"):
     return [node_files, edges, pb_edges, box_size, len_of_chain]
 
 
+def unwrap_nodes(start, end, box_size):
+    for i in range(len(start)):
+        diff = np.abs(end[i] - start[i])
+        if diff > (box_size-diff):
+            end[i] -= box_size
+    return end
+
+
 def upside_down_trapezoid(start, end, number_of_beads, short_base):
     possible_shapes = []
     for long_base in np.arange(1, (number_of_beads + short_base)/2, 2):
         side = (number_of_beads - long_base)/2
-        if (long_base%1 == 0) and (side%1 == 0):
+        if (long_base % 1 == 0) and (side % 1 == 0):
             possible_shapes.append([long_base, side])
     long_base, side = random.choice(possible_shapes)
 
@@ -46,16 +54,16 @@ def upside_down_trapezoid(start, end, number_of_beads, short_base):
 
     base_vector = end - start
     base_length = np.linalg.norm(base_vector)
-        
-    if abs(base_length - short_base) > 1e-6:
-      print("Error: provided base length does not match side_lengths[0]")
-      return None
-    
-    normal_vector = np.cross(base_vector, np.array([0, 0, 1]))
-    if np.allclose(normal_vector, np.array([0,0,0])):
-      normal_vector = np.cross(base_vector, np.array([0, 1, 0]))
 
-    normal_vector = normal_vector / np.linalg.norm(normal_vector) 
+    if abs(base_length - short_base) > 1e-6:
+        print("Error: provided base length does not match side_lengths[0]")
+        return None
+
+    normal_vector = np.cross(base_vector, np.array([0, 0, 1]))
+    if np.allclose(normal_vector, np.array([0, 0, 0])):
+        normal_vector = np.cross(base_vector, np.array([0, 1, 0]))
+
+    normal_vector = normal_vector / np.linalg.norm(normal_vector)
 
     delta = np.cos(alpha)
     v3 = start + delta*base_vector + height*normal_vector
@@ -68,7 +76,8 @@ def upside_down_trapezoid(start, end, number_of_beads, short_base):
 def create_positions(points, lengths, number_of_beads):
     [start, v3, v4, end] = points
     [short_base, long_base, side] = lengths
-    AtomPositions=pd.DataFrame(data=np.zeros([number_of_beads+2, 3]), columns=["X", "Y", "Z"], index = np.arange(0, number_of_beads+2))
+    AtomPositions = pd.DataFrame(data=np.zeros(
+        [number_of_beads+2, 3]), columns=["X", "Y", "Z"], index=np.arange(0, number_of_beads+2))
     generated_positions = np.linspace(start, v3, int(side)+1)
     for i in range(int(side)+1):
         print(i)
@@ -82,13 +91,16 @@ def create_positions(points, lengths, number_of_beads):
         AtomPositions.loc[int(side) + i, "X"] = generated_positions[i, 0]
         AtomPositions.loc[int(side) + i, "Y"] = generated_positions[i, 1]
         AtomPositions.loc[int(side) + i, "Z"] = generated_positions[i, 2]
-    
+
     generated_positions = np.linspace(v4, end, int(side) + 2)
     for i in range(int(side) + 2):
         print(int(side) + int(long_base) + i)
-        AtomPositions.loc[int(side) + int(long_base) + i, "X"] = generated_positions[i, 0]
-        AtomPositions.loc[int(side) + int(long_base) + i, "Y"] = generated_positions[i, 1]
-        AtomPositions.loc[int(side) + int(long_base) + i, "Z"] = generated_positions[i, 2]
+        AtomPositions.loc[int(side) + int(long_base) + i,
+                          "X"] = generated_positions[i, 0]
+        AtomPositions.loc[int(side) + int(long_base) + i,
+                          "Y"] = generated_positions[i, 1]
+        AtomPositions.loc[int(side) + int(long_base) + i,
+                          "Z"] = generated_positions[i, 2]
     print(AtomPositions)
 
     fig = plt.figure()
@@ -101,8 +113,10 @@ def create_positions(points, lengths, number_of_beads):
 
     plt.show()
 
+
 def trapezoid():
     pass
+
 
 def init_shape_creation(start, end, number_of_beads, box_size):
     """Generate a chain that forms a trapezoid or triangle."""
@@ -119,36 +133,38 @@ def init_shape_creation(start, end, number_of_beads, box_size):
     dist = np.sqrt(dx**2 + dy**2 + dz**2)
     print(vector, dist)
     bond_length = 0.97
-
-    end = unwrap_positions(end)
+    print(start, end)
+    end = unwrap_nodes(end)
+    print(end)
     if number_of_beads % 2:
         print('ODD')
         if (dist > 0) and (dist <= bond_length):
             points = upside_down_trapezoid()
         elif dist > bond_length <= 2*bond_length:
             points = trapezoid()
-        
+
     else:
         print('EVEN')
     return vector
 
+
 def select_chain(full_edge_data, bond_data, bead_data, node_data):
     """Create chains."""
-    [node_x, node_y, node_z, node_type] = [node_data[:,0], node_data[:,1],
-                                           node_data[:,2], node_data[:,3]]
+    [node_x, node_y, node_z, node_type] = [node_data[:, 0], node_data[:, 1],
+                                           node_data[:, 2], node_data[:, 3]]
     colors = mpl.colormaps['rainbow'](np.linspace(0, 1, len(full_edge_data)))
     plt.figure()
     fullcycle = np.zeros([len(full_edge_data), 2])
     for chain, edge in enumerate(full_edge_data):
         point_0 = np.array([node_x[int(edge[0])], node_y[int(edge[0])],
-                   node_z[int(edge[0])]])
+                            node_z[int(edge[0])]])
         point_n = np.array([node_x[int(edge[1])], node_y[int(edge[1])],
-                   node_z[int(edge[1])]])
+                            node_z[int(edge[1])]])
         maxdist = 50  # arbitrary large value to start
         cycle = 0
         while maxdist > 3 and cycle < 25:  # arbitrary cut offs
             path = init_shape_creation(start=point_0, end=point_n,
-                             number_of_beads=LENGTH_OF_CHAIN, box_size=BOX_SIZE)
+                                       number_of_beads=LENGTH_OF_CHAIN, box_size=BOX_SIZE)
             path_x = path[:, 0]
             path_y = path[:, 1]
             curr = 1
@@ -184,14 +200,16 @@ def select_chain(full_edge_data, bond_data, bead_data, node_data):
 
 # %%
 
+
 STUDY_NAME = '20241016B1C1'
 
 [NodeData, Edges, PB_edges, BOX_SIZE, LENGTH_OF_CHAIN] = load_files(STUDY_NAME)
 FullEdges = np.concatenate((Edges, PB_edges))
 BeadData = file_functions.create_atom_list_3D(NodeData, FullEdges,
-                                           LENGTH_OF_CHAIN)
+                                              LENGTH_OF_CHAIN)
 BondData = pd.DataFrame(columns=["BondType", "Atom1", "Atom2"], dtype="int")
 step = 1/(2*np.sqrt(3))
-points, lengths = upside_down_trapezoid(np.array([1, 1, 5]), np.array([1+step, 1+step, 5+step]),25, 0.5)
+points, lengths = upside_down_trapezoid(
+    np.array([1, 1, 5]), np.array([1+step, 1+step, 5+step]), 25, 0.5)
 create_positions(points, lengths, 25)
 # BeadData, BondData = select_chain(FullEdges, BondData, BeadData, NodeData)
